4cef5039f02e6eba4ff5e8326385a93cb849b940
dachanh <ghostcodert7@gmail.com>
2019-05-13 11:44:07 +0700

    template matching

--- orb_detected_traffic_sign.py
+++ orb_detected_traffic_sign.py
@@ -1,115 +1,61 @@
 import cv2
-import imutils
 import numpy as np
 
-def get_feature(candidate,template):
-    dectector =cv2.ORB()
-    keyPoint_template , descriptior_template = dectector.detectAndCompute(template,None)
-    keyPoint_candidate , descriptior_candidate = dectector.detectAndCompute(candidate,None)
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(descriptior_candidate,descriptior_template,k=2)
-    countGoodPoints = 0 
-    for m , n in matches:
-        if (m.distance < 0.75*n.distance):
-            countGoodPoints +=1
-    return countGoodPoints
 
-def detected_traffic_sign(img,img_LeftSign,img_RightSign):
-    candidate_width , candidate_height = img.shape[:2]
-    template_LeftSign_width , template_LeftSign_height = img_LeftSign.shape[:2]
-    template_RightSign_width , template_RightSign_height = img_RightSign.shape[:2]
-    average_width = int((candidate_width + template_LeftSign_width + template_RightSign_width)/3)
-    average_height = int((candidate_height + template_LeftSign_height + template_RightSign_height)/3)
-    img_LeftSign = cv2.resize(img_LeftSign,(average_height,average_width))
-    img_RightSign = cv2.resize(img_RightSign,(average_height,average_width))
-    img = cv2.resize(img,(average_height,average_width))
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    _,img = cv2.threshold(img,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    result = 0
-    goodPointsLeftSign = get_feature(img,img_LeftSign)
-    goodPointsRightSign = get_feature(img,img_RightSign)
-    if (goodPointsLeftSign < goodPointsRightSign):
-        result = 1 
-    else:
-        if (goodPointsLeftSign > goodPointsRightSign):
-            result = 2
-        else:
-            result = 0
-    return result
+class parking_sign_detector(object):
+    def __init__(self):
+        self.template = cv2.imread('./parking_sign.png',0)
+        self.dectector =cv2.ORB_create()
+        self.kps_1 , self.des_1 = self.dectector.detectAndCompute(self.template,None)
+        self.bf = cv2.BFMatcher()
+        self.GOOD_SCORES = 15
+    def find_contour(self,img):
+        temp = img.copy()
+        _,contours,_ = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        candidates = []
+        for cnt in contours:
+            approx = cv2.approxPolyDP(cnt,0.05*cv2.arcLength(cnt,True),True)
+            area = cv2.contourArea(cnt)
+            rect = cv2.boundingRect(cnt)
+            if (len(approx) > 8) & (len(approx) < 15) & (area > 30) & (rect[2] < 300) & (rect[3] < 300) & (rect[2] >= 30) or (rect[3] >= 30):
+                x,y,w,h = rect
+                candidates.append([x,y,w,h])
+        return candidates
+    def blue_extraction(self,img):
+        img = cv2.GaussianBlur(img,(5,5),0)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        blue =  cv2.inRange(hsv, np.array([100,150,0]), np.array([140,255,255]))
+        kernel = np.ones((3,3),np.uint8)
+        close = cv2.morphologyEx(blue, cv2.MORPH_CLOSE, kernel)
+        openc = cv2.morphologyEx(close, cv2.MORPH_OPEN, kernel)
+        openc = cv2.GaussianBlur(openc,(9,9),2,2)
+        return openc 
+    def matching_points(self,img):
+        temp = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        _,temp =cv2.threshold(temp,125,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU) 
+        kps_2 , des_2 = self.dectector.detectAndCompute(temp,None)
+        matches = self.bf.knnMatch(des_2,self.des_1,k=2)
+        counting_scores = 0 
+        for m , n in matches:
+            if m.distance < 0.75*n.distance : counting_scores +=1
+        return counting_scores
 
-
-def find_contour(img):
-    temp = img.copy()
-    _,contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    candidates = []
-    for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.05*cv2.arcLength(cnt,True),True)
-        area = cv2.contourArea(cnt)
-        rect = cv2.boundingRect(cnt)
-        if (len(approx) > 8) & (len(approx) < 15) & (area > 30) & (rect[2] < 300) & (rect[3] < 300) & (rect[2] >= 30) or (rect[3] >= 30):
-            x,y,w,h = rect
-            candidates.append([x,y,w,h])
-    return candidates
-def fill_hole(img):
-    img_floodfill = img.copy()
-    h , w  = img_floodfill.shape[:2]
-    mask = np.zeros((h+2,w+2),np.uint8)
-    cv2.floodFill(img_floodfill,mask,(0,0),255)
-    img_floodfill_inv = cv2.bitwise_not(img_floodfill)
-    img_out = img | img_floodfill_inv
-    return img_out
-def removeSmallComponents(image, threshold):
-    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
-    sizes = stats[1:, -1]; nb_components = nb_components - 1
-    img2 = np.zeros((output.shape),dtype = np.uint8)
-    for i in range(0, nb_components):
-        if sizes[i] >= threshold:
-            img2[output == i + 1] = 255
-    return img2
-def blue_extraction(img):
-    img = cv2.GaussianBlur(img,(5,5),0)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    blue =  cv2.inRange(hsv, np.array([100,150,0]), np.array([140,255,255]))
-    kernel = np.ones((3,3),np.uint8)
-    close = cv2.morphologyEx(blue, cv2.MORPH_CLOSE, kernel)
-    openc = cv2.morphologyEx(close, cv2.MORPH_OPEN, kernel)
-    openc = cv2.GaussianBlur(openc,(9,9),2,2)
-    return openc
-
-def main():
-    cap =cv2.VideoCapture("./localization_traffic_sign/1.mp4")
-    img_LeftSign = cv2.imread('/parking_sign.png')
-    numframe = -1 
-    while(cap.isOpened()):
-        _,img = cap.read()
-        numframe = numframe + 1  
-        frame_1 = blue_extraction(img)
-        frame_1 = removeSmallComponents(frame_1,50)
-        frame_1 = fill_hole(frame_1)
-        #cv2.imshow("frame 1",frame_1)
-        candidates = find_contour(frame_1)
-        #print(candidates)
-        count = 0
-        for candidate in candidates:
-            print(candidate)
-            count = count + 1
-            x,y,w,h = candidate[:4]
-            subImg = img[y:y+h,x:x+w,]
-            nameOfTrafficSign = detected_traffic_sign(subImg,img_LeftSign,img_RightSign)
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255, 255, 00), 2)
-            if nameOfTrafficSign == 1:
-                text ="Right Sign"
+    def detectAndCompute(self,img):
+        temp =img.copy()
+        temp = self.blue_extraction(temp)
+        cnts = self.find_contour(temp)
+        for cnt in cnts:
+            x,y,w,h = cnt
+            temp = img[y:y+h,x:x+w] 
+            cv2.imshow("sdsd",temp)
+            cv2.waitKey(0)
+            scores = self.matching_points(temp) 
+            print("score",scores)
+            if scores >= self.GOOD_SCORES:
+                print("Parking Sign")
             else:
-                if nameOfTrafficSign == 0 :
-                    text = "Left Sign"
-                else:
-                    text = "unidentified"
-            cv2.putText(img, text, (x, h), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA) 
-        img = cv2.resize(img,(800,800))
-        cv2.imshow("result",img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-if __name__ == "__main__":
-    main()
+                print("No Parking Sign")
+
+
+#detector = parking_sign_detector()
+#detector.detectAndCompute(cv2.imread('./sign.png'))
--- template_matching.py
+++ template_matching.py
@@ -1,25 +1,55 @@
 #! /usr/bin/env python
 import numpy as np
+import glob
+import os
 import cv2
 
-class TemplateMatching(object):
+class Template_matching(object):
     def __init__(self):
-        self.template = cv2.imread('./parking_sign.png',0)
-        self.edged_template = self.auto_canny(template) 
-    def auto_canny(self,img,sigma = 0.33):
-        v = np.mean(img)
-        lower = int(max(0,(1.0-sigma)*v))
-        upper =int(min(255,(1.0 + sigma)*v))
-        edged = cv2.Canny(img,lower,upper)
-        return edged
-
+        self.template = cv2.imread('./parking_sign.png',0) 
+    def resize(self,image, width = None, height = None, inter = cv2.INTER_AREA):
+        dim = None
+        (h, w) = image.shape[:2]
+        if width is None and height is None:
+            return image
+        if width is None:
+            r = height / float(h)
+            dim = (int(w * r), height)
+        else:
+            r = width / float(w)
+            dim = (width, int(h * r))
+        resized = cv2.resize(image, dim, interpolation = inter)
+        return resized
     def detectAndCompute(self,img):
-        query_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        edged_query_img = self.auto_canny(query_img)
-        h,w = edged_query_img.shape[0],edged_query_img.shape[1]
-        #multi scale 
-        for scale in np.linspace(0.2,1.0,20)[::-1]:
-            resized = cv2.resize(gray,(w*scale,h),interpolation=cv2.INTER_CUBIC)
-            result = cv2.matchTemplate(resized,self.edged_template,cv2.TM_CCOEFF)
-            (_,maxVal,_maxLoc) = cv2.minMaxLoc(result)
-            
+        (tH, tW) = self.template.shape[:2]
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray,(3,3),0)
+        gray = cv2.Laplacian(blur,cv2.CV_64F)
+        gray = np.float32(gray)
+        found = None
+        for scale in np.linspace(0.5, 2, 30):
+            resized = self.resize(gray, width = int(gray.shape[1] * scale))
+            r = gray.shape[1] / float(resized.shape[1])
+            if resized.shape[0] < tH or resized.shape[1] < tW:
+                break
+            result = cv2.matchTemplate(resized, self.template, cv2.TM_CCOEFF)
+            (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+            if found is None or maxVal > found[0]:
+                found = (maxVal, maxLoc, r)
+                (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+                (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+        if found is not None:
+            #print(imagePath,'maxval=',maxVal,'Cursor detected at location:',(int(maxLoc[0] * r), int(maxLoc[1] * r)))
+            (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+            (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+            cv2.rectangle(img, (startX, startY), (endX, endY), (0, 0, 255), 2)
+            cv2.imshow('img',img)
+            return (1,[startX,startY,endX,endY])
+        return (-1,None)
+
+
+detector = Template_matching()
+img = cv2.imread('./parking_2.png')
+res,loc= detector.detectAndCompute(img)
+print(res)
+
